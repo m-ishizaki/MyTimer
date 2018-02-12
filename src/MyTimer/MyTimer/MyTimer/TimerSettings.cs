be3701121ec6cec0f29f279ab315cb26f133ab4c
m ishizaki <love.ms.is12t@hotmail.co.jp>
2018-02-13 01:37:44 +0900

    コメントを追加

--- src/MyTimer/MyTimer/MyTimer/TimerSettings.cs
+++ src/MyTimer/MyTimer/MyTimer/TimerSettings.cs
@@ -4,14 +4,22 @@ using System.Text;
 
 namespace MyTimer
 {
+    // タイマー設定保持クラス
     class TimerSettings
     {
+        // アプリ内で単一のインスタンスを取得する
+        // タイマー設定はアプリで一つだけ保持する
         public static TimerSettings Instance { get; } = new TimerSettings();
 
+        // コンストラクタ
+        // 外部でインスタンス化できないよう private にする
         private TimerSettings() {; }
 
+        // カウントする時間を取得または設定する
         public double CountMilliseconds { get; set; }
+        // カウント終了時に読み上げるテキストを取得または設定する
         public string SpeechText { get; set; }
+        // カント終了時にテキストを読み上げるか否かを取得または設定する
         public bool UseSpeechText { get; set; }
     }
 }
