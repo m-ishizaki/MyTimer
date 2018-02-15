8b434d739b239d98a1ea469537bdc903eedb6770
m ishizaki <love.ms.is12t@hotmail.co.jp>
2018-02-16 01:34:26 +0900

    コメントを整備

--- src/MyTimer/MyTimer/MyTimer/ViewModels/CountDownPageViewModel.cs
+++ src/MyTimer/MyTimer/MyTimer/ViewModels/CountDownPageViewModel.cs
@@ -6,10 +6,13 @@ using Xamarin.Forms;
 
 namespace MyTimer.ViewModels
 {
+    // タイマーカウント画面の ViewModel
     class CountDownPageViewModel : BindableBase
     {
+        // タイマーカウント用のストップウォッチ
         private System.Diagnostics.Stopwatch _stopwatch = new System.Diagnostics.Stopwatch();
 
+        // カウントの残り時間
         private TimeSpan _time;
         public TimeSpan Time
         {
@@ -17,21 +20,32 @@ namespace MyTimer.ViewModels
             private set { SetProperty(ref _time, value); }
         }
 
+        // 戻るボタンが押された
         public Command GoBackCommand { get; }
 
+        // 画面を閉じ、カウント設定画面へ戻る
         private void GoBack()
         {
+            // 画面を「戻る」メッセージを送信
             MessagingCenter.Send(this, "GoBack");
         }
 
+        // コンストラクタ
         public CountDownPageViewModel()
         {
+            // コマンドの設定
+            // readonly プロパティの初期化は、コンストラクタ内でも行える
             GoBackCommand = new Command(GoBack);
 
+            // ストップウォッチをスタート
             _stopwatch.Start();
+            // カウントダウン更新タイマーをスタート
             Xamarin.Forms.Device.StartTimer(TimeSpan.FromMilliseconds(33), OnTimerTick);
         }
 
+        // 画面表示を更新
+        // カウントダウン中の残り時間の画面表示を更新する
+        // タイマー処理のたびに呼び出される
         private void UpdateTime()
         {
             var time = Math.Max(0, (TimerSettings.Instance.CountMilliseconds - _stopwatch.ElapsedMilliseconds));
@@ -39,18 +53,26 @@ namespace MyTimer.ViewModels
             Time = TimeSpan.FromMilliseconds(time);
         }
 
+        // タイマー処理
+        // 毎回のタイマーイベントの処理
         private bool OnTimerTick()
         {
+            // 残り時間を更新
             UpdateTime();
+            // 時間が残っている場合、タイマーを続行
             if (Time.TotalMilliseconds > 0)
                 return true;
 
+            // 時間が経過しきった場合、一回だけ動かすタイマーをスタートし、本タイマーは終了する
             Xamarin.Forms.Device.StartTimer(TimeSpan.FromMilliseconds(100), OnTimerEnd);
             return false;
         }
 
+        // 時間が経過しきった後の、一度だけ動かすタイマーの処理
         private bool OnTimerEnd()
         {
+            // タイマー設定を確認し
+            // テキストの読み上げ、または音声ファイルの再生を行う
             if (TimerSettings.Instance.UseSpeechText)
                 TextToSpeech();
             else
@@ -58,11 +80,13 @@ namespace MyTimer.ViewModels
             return false;
         }
 
+        // テキストの読み上げ
         private async void TextToSpeech()
         {
             await Plugin.TextToSpeech.CrossTextToSpeech.Current.Speak(TimerSettings.Instance.SpeechText, volume:0.5f);
         }
 
+        // 音声ファイルの再生
         private void PlayAudio()
         {
             try
@@ -83,6 +107,7 @@ namespace MyTimer.ViewModels
             }
         }
 
+        // 音声ファイルのリソース名指定に誤りがないかチェックをする
         private bool IsContainsResource(System.Reflection.Assembly assembly, string name)
         {
             if (assembly.GetManifestResourceNames().Contains(name)) return true;
